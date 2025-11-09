24de4beabe64cdf3aad2fc7fcd89218f4f54aab6
itsuki <itsuki.enjyoy@gmail.com>
2025-11-09 16:57:10 +0900

    Initial Commit

---
+++ InAppPurchaseDemo/InAppPurchaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppPurchaseDemoApp.swift
+//  InAppPurchaseDemo
+//
+//  Created by Itsuki on 2025/11/09.
+//
+
+import SwiftUI
+
+@main
+struct InAppPurchaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
